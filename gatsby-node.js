c871f56046c565d8941f6ec057120713b6434e10
Matt Kane <matt@gatsbyjs.com>
2020-05-27 20:50:28 +0100

    Use node 12, and null check

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,12 @@
 const path = require(`path`);
 const { emitter } = require("gatsby/dist/redux");
 const component = path.resolve(`./src/templates/work.js`);
-
+let count = 0;
 exports.onCreateNode = ({ node, actions }) => {
-  if (node.internal.type === "DatoCmsBlogPost") {
+  if (node && node.internal && node.internal.type === "DatoCmsBlogPost") {
+    if (count++ % 100 === 0) {
+      console.log(`Page ${count}`);
+    }
     actions.createPage({
       path: `/works/${node.slug}`,
       component,
@@ -16,7 +19,11 @@ exports.onCreateNode = ({ node, actions }) => {
 
 exports.onPreBootstrap = ({ actions }) => {
   emitter.on(`DELETE_NODE`, ({ payload }) => {
-    if (payload.internal.type === `DatoCmsBlogPost`) {
+    if (
+      payload &&
+      payload.internal &&
+      payload.internal.type === `DatoCmsBlogPost`
+    ) {
       actions.deletePage({
         path: `/works/${payload.slug}`,
         component,
